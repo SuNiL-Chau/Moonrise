df73c0fef70562c73b055c8c2e03ee8305675cec
Sagar Maurya <sagar68742@gmail.com>
2023-06-04 20:16:22 +0530

    Home Page mbl Updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let hamburger = document.querySelector(".navbar__hamburger");
+let MenuItems = document.querySelector(".navbar__items");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  MenuItems.classList.toggle("active");
+  if (hamburger.classList.contains("active")) {
+    document.body.classList.add("overflow-h");
+  } else {
+    document.body.classList.remove("overflow-h");
+  }
+});
+//# sourceMappingURL=main.js.map
