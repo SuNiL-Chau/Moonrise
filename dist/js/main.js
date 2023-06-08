f51074b86e45dbc92355391348d8165c427eb0be
Sagar Maurya <sagar68742@gmail.com>
2023-06-08 09:35:42 +0530

    Popup Response Status Updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,7 @@
 "use strict";
 
-const {
-  document
-} = require("postcss");
+// const { document } = require("postcss");
+
 let hamburger = document.querySelector(".navbar__hamburger");
 let MenuItems = document.querySelector(".navbar__items");
 hamburger.addEventListener("click", () => {
@@ -13,5 +12,19 @@ hamburger.addEventListener("click", () => {
   } else {
     document.body.classList.remove("overflow-h");
   }
+});
+let talkButton = document.querySelector(".navbar__talkBtn");
+let closeButton = document.querySelector(".popUp__closeBtn");
+talkButton.addEventListener("click", () => {
+  console.log("add");
+  if (hamburger.classList.contains("active")) {
+    hamburger.classList.toggle("active");
+  }
+  document.body.classList.add("overflow-h");
+  document.querySelector("#letsTalk").classList.add("active");
+});
+closeButton.addEventListener("click", () => {
+  document.body.classList.remove("overflow-h");
+  document.querySelector("#letsTalk").classList.remove("active");
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const { document } = require("postcss");
+// const { document } = require("postcss");
 
 let hamburger = document.querySelector(".navbar__hamburger");
 let MenuItems = document.querySelector(".navbar__items");
@@ -10,4 +10,20 @@ hamburger.addEventListener("click", () => {
 	} else {
 		document.body.classList.remove("overflow-h");
 	}
+});
+let talkButton = document.querySelector(".navbar__talkBtn");
+let closeButton = document.querySelector(".popUp__closeBtn");
+
+talkButton.addEventListener("click", () => {
+	console.log("add");
+	if (hamburger.classList.contains("active")) {
+		hamburger.classList.toggle("active");
+	}
+	document.body.classList.add("overflow-h");
+	document.querySelector("#letsTalk").classList.add("active");
+});
+
+closeButton.addEventListener("click", () => {
+	document.body.classList.remove("overflow-h");
+	document.querySelector("#letsTalk").classList.remove("active");
 });
